c4435744da43737457d4cf102869ec517e32a1ab
Murat <murat.yilmaz@iys.org.tr>
2021-02-20 01:02:42 +0300

    add: topla API

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post("/topla", async (req, res) => {
+    try {
+        const { a, b } = req.body;
+        res.json({ payload: { c: a + b } })
+    } catch (error) {
+        res.status(400).end()
+    }
+})
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
