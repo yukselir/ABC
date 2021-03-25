3920906af38a5d6aab8a70564347d6bbf29cc0f5
Murat <murat.yilmaz@iys.org.tr>
2021-03-25 20:06:21 +0300

    add: truss 2D with min params

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 const { PythonShell } = require('python-shell');
 const config = require("../config/config");
@@ -9,8 +9,8 @@ let options = {
     mode: 'text',
     pythonPath: config.get("pythonPath"),
     pythonOptions: ['-u'], // get print results in real-time
-    scriptPath: './truss2D',
-    args: ['value1', 'value2', 'value3','value4']
+    scriptPath: './fem',
+    args: ['value1', 'value2', 'value3', 'value4']
 };
 
 app.use(express.static('public'));
@@ -29,8 +29,8 @@ const deneme = (req, res, next) => {
 
 
 app.post("/run", (req, res) => {
-    const { a,b } = req.body;
-    PythonShell.run('/topla.py', {...options, args: [a,b]}, function (err, results) {
+    const { a, b } = req.body;
+    PythonShell.run('/topla.py', { ...options, args: [a, b] }, function (err, results) {
         if (err) {
             return res.send(err);
         }
@@ -39,8 +39,21 @@ app.post("/run", (req, res) => {
     });
 })
 
+app.post("/truss2D/solve", (req, res) => {
+    const { h, dLx, N } = req.body;
+
+    PythonShell.run('/truss2D.py', { ...options, args: [h, dLx, N] }, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        const rjson = JSON.parse(results);
+        return res.send({ ok: true, payload: rjson });
+    });
+})
+
 app.post("/truss2D", (req, res) => {
-    const { 
+    const {
         node1x, node1y, node1rx, node1ry,
         node2x, node2y, node2rx, node2ry,
         node3x, node3y, node3rx, node3ry,
@@ -52,20 +65,22 @@ app.post("/truss2D", (req, res) => {
         Elm4ni, Elm4nj, Elm4EA,
         Elm5ni, Elm5nj, Elm5EA,
         Elm6ni, Elm6nj, Elm6EA,
-        Elm7ni, Elm7nj, Elm7EA  } = req.body;
-    PythonShell.run('/main.py', {...options, args: [
-        node1x, node1y, node1rx, node1ry,
-        node2x, node2y, node2rx, node2ry,
-        node3x, node3y, node3rx, node3ry,
-        node4x, node4y, node4rx, node4ry,
-        node5x, node5y, node5rx, node5ry,
-        Elm1ni, Elm1nj, Elm1EA,
-        Elm2ni, Elm2nj, Elm2EA,
-        Elm3ni, Elm3nj, Elm3EA,
-        Elm4ni, Elm4nj, Elm4EA,
-        Elm5ni, Elm5nj, Elm5EA,
-        Elm6ni, Elm6nj, Elm6EA,
-        Elm7ni, Elm7nj, Elm7EA  ]}, function (err, results) {
+        Elm7ni, Elm7nj, Elm7EA } = req.body;
+    PythonShell.run('/main.py', {
+        ...options, args: [
+            node1x, node1y, node1rx, node1ry,
+            node2x, node2y, node2rx, node2ry,
+            node3x, node3y, node3rx, node3ry,
+            node4x, node4y, node4rx, node4ry,
+            node5x, node5y, node5rx, node5ry,
+            Elm1ni, Elm1nj, Elm1EA,
+            Elm2ni, Elm2nj, Elm2EA,
+            Elm3ni, Elm3nj, Elm3EA,
+            Elm4ni, Elm4nj, Elm4EA,
+            Elm5ni, Elm5nj, Elm5EA,
+            Elm6ni, Elm6nj, Elm6EA,
+            Elm7ni, Elm7nj, Elm7EA]
+    }, function (err, results) {
         if (err) {
             return res.send(err);
         }
@@ -91,5 +106,3 @@ app.post("/topla", [deneme], async (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-console.log("SOOOOOOOOOOOOOOOOOOOOOOON")
